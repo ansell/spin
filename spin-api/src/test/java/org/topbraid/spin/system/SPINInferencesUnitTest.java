6041653972a2db9acdc45fdc760c15e74f73cad6
Peter Ansell <p_ansell@yahoo.com>
2011-11-09 15:10:59 +1000

    add two new empty tests for SPINInferences and SPINQueryFinder that need to be tested to verify they work properly with validFunctionSources

---
+++ spin-api/src/test/java/org/topbraid/spin/system/SPINInferencesUnitTest.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package org.topbraid.spin.system;
+
+import org.junit.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ *
+ */
+public class SPINInferencesUnitTest
+{
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#isRuleProperty(com.hp.hpl.jena.rdf.model.Property)}.
+     */
+    @Ignore
+    @Test
+    public void testIsRuleProperty()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#run(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Model, org.topbraid.spin.inference.SPINExplanations, java.util.List, boolean, org.topbraid.base.progress.ProgressMonitor)}.
+     */
+    @Ignore
+    @Test
+    public void testRunModelModelSPINExplanationsListOfSPINStatisticsBooleanProgressMonitor()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#run(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Property, com.hp.hpl.jena.rdf.model.Model, org.topbraid.spin.inference.SPINExplanations, java.util.List, boolean, org.topbraid.base.progress.ProgressMonitor)}.
+     */
+    @Ignore
+    @Test
+    public void testRunModelPropertyModelSPINExplanationsListOfSPINStatisticsBooleanProgressMonitor()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#run(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Property, com.hp.hpl.jena.rdf.model.Model, org.topbraid.spin.inference.SPINExplanations, java.util.List, boolean, org.topbraid.base.progress.ProgressMonitor, java.util.Set)}.
+     */
+    @Ignore
+    @Test
+    public void testRunModelPropertyModelSPINExplanationsListOfSPINStatisticsBooleanProgressMonitorSetOfObject()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#run(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Model, java.util.Map, java.util.Map, java.util.Map, org.topbraid.spin.inference.SPINExplanations, java.util.List, boolean, com.hp.hpl.jena.rdf.model.Property, org.topbraid.spin.inference.SPINRuleComparator, org.topbraid.base.progress.ProgressMonitor)}.
+     */
+    @Ignore
+    @Test
+    public void testRunModelModelMapOfResourceListOfCommandWrapperMapOfResourceListOfCommandWrapperMapOfCommandWrapperMapOfStringRDFNodeSPINExplanationsListOfSPINStatisticsBooleanPropertySPINRuleComparatorProgressMonitor()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#run(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Model, java.util.Map, java.util.Map, java.util.Map, org.topbraid.spin.inference.SPINExplanations, java.util.List, boolean, com.hp.hpl.jena.rdf.model.Property, org.topbraid.spin.inference.SPINRuleComparator, org.topbraid.base.progress.ProgressMonitor, java.util.Set)}.
+     */
+    @Ignore
+    @Test
+    public void testRunModelModelMapOfResourceListOfCommandWrapperMapOfResourceListOfCommandWrapperMapOfCommandWrapperMapOfStringRDFNodeSPINExplanationsListOfSPINStatisticsBooleanPropertySPINRuleComparatorProgressMonitorSetOfObject()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.inference.SPINInferences#runQueryOnInstance(com.hp.hpl.jena.query.Query, com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Resource, boolean, java.util.Map)}.
+     */
+    @Ignore
+    @Test
+    public void testRunQueryOnInstance()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+}
---
+++ spin-api/src/test/java/org/topbraid/spin/system/SPINQueryFinderUnitTest.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package org.topbraid.spin.system;
+
+import org.junit.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Peter Ansell p_ansell@yahoo.com
+ *
+ */
+public class SPINQueryFinderUnitTest
+{
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.util.SPINQueryFinder#add(java.util.Map, com.hp.hpl.jena.rdf.model.Statement, com.hp.hpl.jena.rdf.model.Model, boolean, java.util.Map, boolean)}.
+     */
+    @Ignore
+    @Test
+    public void testAddMapOfResourceListOfCommandWrapperStatementModelBooleanMapOfCommandWrapperMapOfStringRDFNodeBoolean()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.util.SPINQueryFinder#add(java.util.Map, com.hp.hpl.jena.rdf.model.Statement, com.hp.hpl.jena.rdf.model.Model, boolean, java.util.Map, boolean, java.util.Set)}.
+     */
+    @Ignore
+    @Test
+    public void testAddMapOfResourceListOfCommandWrapperStatementModelBooleanMapOfCommandWrapperMapOfStringRDFNodeBooleanSetOfObject()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.util.SPINQueryFinder#getClass2QueryMap(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Property, boolean, java.util.Map, boolean)}.
+     */
+    @Ignore
+    @Test
+    public void testGetClass2QueryMapModelModelPropertyBooleanMapOfCommandWrapperMapOfStringRDFNodeBoolean()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+    /**
+     * Test method for {@link org.topbraid.spin.util.SPINQueryFinder#getClass2QueryMap(com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Model, com.hp.hpl.jena.rdf.model.Property, boolean, java.util.Map, boolean, java.util.Set)}.
+     */
+    @Ignore
+    @Test
+    public void testGetClass2QueryMapModelModelPropertyBooleanMapOfCommandWrapperMapOfStringRDFNodeBooleanSetOfObject()
+    {
+        Assert.fail("Not yet implemented"); // TODO
+    }
+    
+}
